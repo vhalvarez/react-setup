488f7ee0db6db572c8306b096982dc489bfa5579
Victor Alvarez <alvarez3197@gmail.com>
2022-08-10 14:09:33 -0400

    Custom Hooks - useProduct

--- src/component-patterns/components/ProductCard.tsx
+++ src/component-patterns/components/ProductCard.tsx
@@ -1,13 +1,10 @@
+import useProduct from '../hooks/useProduct'
 import styles from "../styles/styles.module.css";
 import noImage from "../assets/no-image.jpg";
-import { useState } from "react";
 
 export const ProductCard = () => {
-    const [counter, setCounter] = useState(0);
-
-    const increaseBy = (value: number) => {
-        setCounter((prev) => Math.max(prev + value, 0));
-    };
+   
+    const {counter, increaseBy} = useProduct()
 
     return (
         <div className={styles.productCard}>
