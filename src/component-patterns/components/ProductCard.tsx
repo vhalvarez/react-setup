2741a09df2ab3b26d06823463006a0fdf9fae795
Victor Alvarez <alvarez3197@gmail.com>
2022-08-15 13:11:15 -0400

    Unificar exportacion de componentes

--- src/component-patterns/components/ProductCard.tsx
+++ src/component-patterns/components/ProductCard.tsx
@@ -67,3 +67,7 @@ export const ProductCard = ({ children, product }: Props) => {
         </div>
     );
 };
+
+ProductCard.Title = ProductTitle
+ProductCard.Image = ProductImage
+ProductCard.Buttons = ProductButtons
--- src/component-patterns/pages/ShoppingPage.tsx
+++ src/component-patterns/pages/ShoppingPage.tsx
@@ -1,9 +1,14 @@
-import { ProductButtons, ProductCard, ProductImage, ProductTitle } from "../components/ProductCard";
+import {
+    ProductButtons,
+    ProductCard,
+    ProductImage,
+    ProductTitle,
+} from "../components/ProductCard";
 
 const product = {
-    id: '1',
-    title: 'Coffe'
-}
+    id: "1",
+    title: "Coffe",
+};
 
 export const ShoppingPage = () => {
     return (
@@ -18,13 +23,23 @@ export const ShoppingPage = () => {
                     flexWrap: "wrap",
                 }}
             >
-                <ProductCard product={product}>
-                    {/* Necesita correciones */}
-                   <ProductImage />
-                   <ProductTitle title={''}/>
-                   <ProductButtons increaseBy={function (value: number): void {
+                {/* <ProductCard product={product}>
+                   <ProductCard.Image />
+                   <ProductCard.Title title={'Cafe'}/>
+                   <ProductCard.Buttons increaseBy={function (value: number): void {
                         throw new Error("Function not implemented.");
                     } } counter={0} />
+                </ProductCard> */}
+
+                <ProductCard product={product}>
+                    <ProductImage />
+                    <ProductTitle title={"Cafe"} />
+                    <ProductButtons
+                        increaseBy={function (value: number): void {
+                            throw new Error("Function not implemented.");
+                        }}
+                        counter={0}
+                    />
                 </ProductCard>
             </div>
         </div>
