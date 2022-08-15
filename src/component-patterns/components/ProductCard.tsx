fa4455002f050a67d3cd9ca3d9b74489a4aefc8f
Victor Alvarez <alvarez3197@gmail.com>
2022-08-15 11:10:18 -0400

    Recibir props al componente hijo

--- src/component-patterns/components/ProductCard.tsx
+++ src/component-patterns/components/ProductCard.tsx
@@ -1,17 +1,26 @@
-import useProduct from '../hooks/useProduct'
+import useProduct from "../hooks/useProduct";
 import styles from "../styles/styles.module.css";
 import noImage from "../assets/no-image.jpg";
 
-export const ProductCard = () => {
-   
-    const {counter, increaseBy} = useProduct()
+interface Props {
+    product: Product;
+}
+
+interface Product {
+    id: string;
+    title: string;
+    img?: string;
+}
+
+export const ProductCard = ({ product }: Props) => {
+    const { counter, increaseBy } = useProduct();
 
     return (
         <div className={styles.productCard}>
-            {/* <img className={styles.productImg} src="./coffee-mug.png" alt="Coffe Mug" /> */}
-            <img className={styles.productImg} src={noImage} alt="Coffe Mug" />
+            
+            <img className={styles.productImg} src={ product.img ? product.img : noImage} alt="Coffe Mug" />
 
-            <span className={styles.productDescription}>Coffe Mug</span>
+            <span className={styles.productDescription}>{ product.title }</span>
 
             <div className={styles.buttonsContainer}>
                 <button
--- src/component-patterns/pages/ShoppingPage.tsx
+++ src/component-patterns/pages/ShoppingPage.tsx
@@ -1,11 +1,25 @@
 import { ProductCard } from "../components/ProductCard";
 
+const product = {
+    id: '1',
+    title: 'Coffe'
+}
+
 export const ShoppingPage = () => {
     return (
         <div>
             <h1>Shopping Store</h1>
             <hr />
-            <ProductCard />
+
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                }}
+            >
+                <ProductCard product={product}/>
+            </div>
         </div>
     );
 };
